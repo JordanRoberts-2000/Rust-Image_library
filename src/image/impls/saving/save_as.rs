9bfaf1cae1bcf17fd21f17b866fb3ae0af946797
Jordan Roberts <jordanroberts333@icloud.com>
2025-06-23 19:12:38 +0100

    fix: save_as no longer permanently alters internal format state

---
+++ src/image/impls/saving/save_as.rs
@@ -0,0 +1,29 @@
+use std::path::Path;
+
+use crate::{Image, ImageError, ImageFormat, Result};
+
+impl Image {
+    pub fn save_as(&mut self, path: impl AsRef<Path>) -> Result<()> {
+        let path = path.as_ref();
+
+        let ext = path
+            .extension()
+            .and_then(|e| e.to_str())
+            .ok_or_else(|| ImageError::ExtensionMissing(path.to_path_buf()))?;
+
+        let format = ImageFormat::try_from(ext)
+            .map_err(|_| ImageError::InvalidExtension(ext.to_string()))?;
+
+        let original_format = self.format;
+        self.format = format;
+
+        let result = (|| {
+            self.apply_transforms()?;
+            self.atomic_save(path)?;
+            Ok(())
+        })();
+
+        self.format = original_format;
+        result
+    }
+}
