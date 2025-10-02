37a055a8d407c1e15e576f524b0128e1f13e6d26
Jordan Roberts <jordanroberts333@icloud.com>
2025-10-02 21:44:10 +0100

    feat: expanded on ImageFormat, more traits and methods

--- src/types/format.rs
+++ src/types/format.rs
@@ -1,12 +1,13 @@
 use {
-    crate::{InnerError, ValidationError},
-    std::path::Path,
-    strum::VariantNames,
-    strum_macros::{Display, VariantNames},
+    crate::{ImageError, ValidationError},
+    std::path::{Path, PathBuf},
+    strum::IntoEnumIterator,
+    strum_macros::EnumIter,
 };
 
-#[derive(Display, Debug, Clone, Copy, VariantNames, PartialEq, Default)]
-#[strum(serialize_all = "lowercase")]
+const SUPPORTED_EXTS: &[&str] = &["webp", "png", "jpeg", "jpg", "avif"];
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Default, EnumIter, Hash)]
 pub enum ImageFormat {
     WebP,
     #[default]
@@ -16,8 +17,17 @@ pub enum ImageFormat {
 }
 
 impl ImageFormat {
-    pub fn supported() -> &'static [&'static str] {
-        ImageFormat::VARIANTS
+    pub fn supported_exts() -> &'static [&'static str] {
+        SUPPORTED_EXTS
+    }
+
+    pub fn variants() -> Vec<Self> {
+        ImageFormat::iter().collect()
+    }
+
+    pub fn is_supported_ext(ext: &str) -> bool {
+        let e = ext.trim_start_matches('.').to_ascii_lowercase();
+        SUPPORTED_EXTS.contains(&e.as_str())
     }
 
     pub fn to_mime_type(&self) -> &'static str {
@@ -37,10 +47,19 @@ impl ImageFormat {
             ImageFormat::Avif => "avif",
         }
     }
+
+    pub fn extensions(&self) -> &'static [&'static str] {
+        match self {
+            ImageFormat::Jpeg => &["jpeg", "jpg"],
+            ImageFormat::Png => &["png"],
+            ImageFormat::WebP => &["webp"],
+            ImageFormat::Avif => &["avif"],
+        }
+    }
 }
 
 impl TryFrom<&str> for ImageFormat {
-    type Error = ();
+    type Error = ImageError;
 
     fn try_from(ext: &str) -> Result<Self, Self::Error> {
         match ext.to_ascii_lowercase().as_str() {
@@ -48,40 +67,22 @@ impl TryFrom<&str> for ImageFormat {
             "png" => Ok(ImageFormat::Png),
             "jpg" | "jpeg" => Ok(ImageFormat::Jpeg),
             "avif" => Ok(ImageFormat::Avif),
-            _ => Err(()),
+            _ => Err(ValidationError::UnsupportedExtension(ext.to_string()).into()),
         }
     }
 }
 
-impl TryFrom<image::ImageFormat> for ImageFormat {
-    type Error = InnerError;
-
-    fn try_from(fmt: image::ImageFormat) -> Result<Self, Self::Error> {
-        match fmt {
-            image::ImageFormat::Png => Ok(ImageFormat::Png),
-            image::ImageFormat::Jpeg => Ok(ImageFormat::Jpeg),
-            image::ImageFormat::WebP => Ok(ImageFormat::WebP),
-            image::ImageFormat::Avif => Ok(ImageFormat::Avif),
-            other => Err(InnerError::UnsupportedFormat(other)),
-        }
+impl TryFrom<String> for ImageFormat {
+    type Error = ImageError;
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        ImageFormat::try_from(s.as_str())
     }
 }
 
-impl From<ImageFormat> for image::ImageFormat {
-    fn from(fmt: ImageFormat) -> image::ImageFormat {
-        match fmt {
-            ImageFormat::WebP => image::ImageFormat::WebP,
-            ImageFormat::Png => image::ImageFormat::Png,
-            ImageFormat::Jpeg => image::ImageFormat::Jpeg,
-            ImageFormat::Avif => image::ImageFormat::Avif,
-        }
-    }
-}
+impl TryFrom<&Path> for ImageFormat {
+    type Error = ImageError;
 
-impl ImageFormat {
-    pub fn try_from_path(path: impl AsRef<Path>) -> Result<Self, InnerError> {
-        let path = path.as_ref();
-
+    fn try_from(path: &Path) -> Result<Self, Self::Error> {
         match path.extension() {
             Some(extension) => {
                 let ext_str = extension
@@ -95,10 +96,43 @@ impl ImageFormat {
                     "jpg" | "jpeg" => Ok(ImageFormat::Jpeg),
                     "png" => Ok(ImageFormat::Png),
                     "webp" => Ok(ImageFormat::WebP),
+                    "avif" => Ok(ImageFormat::Avif),
                     _ => Err(ValidationError::UnsupportedExtension(ext_str).into()),
                 }
             }
             None => Err(ValidationError::MissingExtensionForPath(path.to_path_buf()).into()),
+        }
+    }
+}
+
+impl TryFrom<PathBuf> for ImageFormat {
+    type Error = ImageError;
+    fn try_from(p: PathBuf) -> Result<Self, Self::Error> {
+        ImageFormat::try_from(p.as_path())
+    }
+}
+
+impl TryFrom<image::ImageFormat> for ImageFormat {
+    type Error = ImageError;
+
+    fn try_from(fmt: image::ImageFormat) -> Result<Self, Self::Error> {
+        match fmt {
+            image::ImageFormat::Png => Ok(ImageFormat::Png),
+            image::ImageFormat::Jpeg => Ok(ImageFormat::Jpeg),
+            image::ImageFormat::WebP => Ok(ImageFormat::WebP),
+            image::ImageFormat::Avif => Ok(ImageFormat::Avif),
+            other => Err(ValidationError::UnsupportedFormat(other).into()),
+        }
+    }
+}
+
+impl From<ImageFormat> for image::ImageFormat {
+    fn from(fmt: ImageFormat) -> image::ImageFormat {
+        match fmt {
+            ImageFormat::WebP => image::ImageFormat::WebP,
+            ImageFormat::Png => image::ImageFormat::Png,
+            ImageFormat::Jpeg => image::ImageFormat::Jpeg,
+            ImageFormat::Avif => image::ImageFormat::Avif,
         }
     }
 }
