34f45f86fab44e64855fe62f658e45cb31145a3a
Jordan Roberts <jordanroberts333@icloud.com>
2025-10-20 22:07:53 +0100

    feat: added gif encoding support

--- src/core/image/impls/internal/encode.rs
+++ src/core/image/impls/internal/encode.rs
@@ -1,33 +1,80 @@
 use {
     crate::{
-        encoding::{AvifEncoder, JpegEncoder, PngEncoder, TiffEncoder, WebpEncoder},
-        image::types::ResolvedColorType,
-        Image, ImageFormat, Result,
+        encoding::{
+            AvifColorType, AvifEncoder, ColorType, JpegColorType, JpegEncoder, PngColorType,
+            PngEncoder, TiffColorType, TiffEncoder, WebpColorType, WebpEncoder,
+        },
+        image::Decoded,
+        EncodeFormat, Image, Result, ValidationError,
     },
-    image::GenericImageView,
-    std::io::Write,
+    image::{
+        codecs::gif::{GifEncoder, Repeat},
+        Delay, Frame,
+    },
+    std::{io::Write, time::Duration},
 };
 
 impl Image {
-    pub(crate) fn encode(&self, writer: impl Write, format: ImageFormat) -> Result<()> {
-        let img = self.processed_image();
-        let (w, h) = img.dimensions();
+    pub(crate) fn encode(&self, writer: impl Write, format: EncodeFormat) -> Result<()> {
+        let decoded = self.decoded();
+        let (w, h) = decoded.dimensions();
+        let ct = ColorType::try_from(decoded.color())
+            .map_err(|_| ValidationError::UnsupportedColorType(decoded.color()))?;
 
-        match self.resolve_color_type(&*img, format)? {
-            ResolvedColorType::Jpeg(ct) => {
-                JpegEncoder::from(self.config.jpeg()).encode(writer, ct.bytes(&*img), w, h, ct)
+        match format {
+            EncodeFormat::Jpeg => {
+                let ct = JpegColorType::from_color_type_lossy(ct);
+                let bytes = ColorType::from(ct).bytes(&decoded);
+                JpegEncoder::from(self.config.jpeg()).encode(writer, bytes, w, h, ct)
             }
-            ResolvedColorType::Png(ct) => {
-                PngEncoder::from(self.config.png()).encode(writer, ct.bytes(&*img), w, h, ct)
+            EncodeFormat::Png => {
+                let ct = PngColorType::from_color_type_lossy(ct);
+                let bytes = ColorType::from(ct).bytes(&decoded);
+                PngEncoder::from(self.config.png()).encode(writer, bytes, w, h, ct)
             }
-            ResolvedColorType::Webp(ct) => {
-                WebpEncoder::from(self.config.webp()).encode(writer, ct.bytes(&*img), w, h, ct)
+            EncodeFormat::Webp => {
+                let ct = WebpColorType::from_color_type_lossy(ct);
+                let bytes = ColorType::from(ct).bytes(&decoded);
+                WebpEncoder::from(self.config.webp()).encode(writer, bytes, w, h, ct)
             }
-            ResolvedColorType::Avif(ct) => {
-                AvifEncoder::from(self.config.avif()).encode(writer, ct.bytes(&*img), w, h, ct)
+            EncodeFormat::Avif => {
+                let ct = AvifColorType::from_color_type_lossy(ct);
+                let bytes = ColorType::from(ct).bytes(&decoded);
+                AvifEncoder::from(self.config.avif()).encode(writer, bytes, w, h, ct)
             }
-            ResolvedColorType::Tiff(ct) => TiffEncoder::encode(writer, ct.bytes(&*img), w, h, ct),
-        }?;
+            EncodeFormat::Tiff => {
+                let ct = TiffColorType::from_color_type_lossy(ct);
+                let bytes = ColorType::from(ct).bytes(&decoded);
+                TiffEncoder.encode(writer, bytes, w, h, ct)
+            }
+            EncodeFormat::Gif => {
+                let mut encoder = GifEncoder::new(writer);
+                encoder.set_repeat(Repeat::Infinite).unwrap();
+                match &*decoded {
+                    Decoded::Static(img) => {
+                        let rgba = img.to_rgba8();
+                        let frame = Frame::from_parts(
+                            rgba,
+                            0,
+                            0,
+                            Delay::from_saturating_duration(Duration::from_millis(0)),
+                        );
+                        encoder.encode_frame(frame);
+                        Ok(())
+                    }
+
+                    Decoded::Animated { frames, .. } => {
+                        let (head, tail) = (frames.head.clone(), frames.tail.clone());
+
+                        encoder.encode_frame(head).unwrap();
+                        for fr in tail {
+                            encoder.encode_frame(fr).unwrap();
+                        }
+                        Ok(())
+                    }
+                }
+            }
+        };
 
         Ok(())
     }
